dc87800cc3b057753c3037ae0ac2e22f9fdddfc8
Risumi <rizkysuhaimi@gmail.com>
2019-05-03 22:38:30 +0700

    added initial firebase cloud

--- app/src/main/java/com/example/saveme/Login.java
+++ app/src/main/java/com/example/saveme/Login.java
@@ -1,25 +1,63 @@
 package com.example.saveme;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Login extends AppCompatActivity implements View.OnClickListener{
     Button btnLogin;
-
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+//        FirebaseApp.initializeApp(this);
+        FirebaseApp.initializeApp(this);
     }
-
     @Override
     public void onClick(View v) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+        db = FirebaseFirestore.getInstance();
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Succes : ", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Failure : ", "Error adding document", e);
+                    }
+                });
+//        Intent i = new Intent(this, MainActivity.class);
+//        startActivity(i);
+
     }
 }
