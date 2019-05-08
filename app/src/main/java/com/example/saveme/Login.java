1ebc0ca5b17c32ff5ce2fa5fcbd64283aa0c2fa0
Risumi <rizkysuhaimi@gmail.com>
2019-05-08 23:03:18 +0700

    added login and register

--- app/src/main/java/com/example/saveme/Login.java
+++ app/src/main/java/com/example/saveme/Login.java
@@ -7,23 +7,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
-    Button btnLogin;
+    Button btnLogin, btnRegister;
+    EditText etUsername, etPassword;
     FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +40,46 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.login);
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword= findViewById(R.id.etPassword);
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(this);
+
 //        FirebaseApp.initializeApp(this);
         FirebaseApp.initializeApp(this);
     }
     @Override
     public void onClick(View v) {
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-        db = FirebaseFirestore.getInstance();
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("Succes : ", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("Failure : ", "Error adding document", e);
-                    }
-                });
-//        Intent i = new Intent(this, MainActivity.class);
-//        startActivity(i);
-
+        if (v == btnLogin){
+            db = FirebaseFirestore.getInstance();
+            db.collection("user")
+                    .whereEqualTo("username",etUsername.getText().toString())
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                String password="";
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d("Success", document.getId() + " => " + document.getData());
+                                    password = document.getData().get("password").toString();
+                                }
+                                if (etPassword.getText().toString().equals(password)){
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                }
+                            } else {
+                                Log.w("Failure : ", "Error getting documents.", task.getException());
+                            }
+                        }
+                    });
+//            Intent intent = new Intent(this, MainActivity.class);
+//            startActivity(intent);
+        }else if (v == btnRegister){
+            Intent intent = new Intent(this, Register.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/saveme/Register.java
+++ app/src/main/java/com/example/saveme/Register.java
@@ -1,16 +1,27 @@
 package com.example.saveme;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
     Button btnSubmit;
     EditText etDaftarNama, etDaftarUsername, etDaftarPass;
-
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +33,30 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         etDaftarNama = findViewById(R.id.etDaftarNama);
         etDaftarUsername = findViewById(R.id.etDaftarUsername);
         etDaftarPass = findViewById(R.id.etDaftarPass);
-        etDaftarNama.setOnClickListener(this);
-        etDaftarUsername.setOnClickListener(this);
-        etDaftarPass.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", etDaftarNama.getText().toString());
+        user.put("username", etDaftarUsername.getText().toString());
+        user.put("password", etDaftarPass.getText().toString());
+        db = FirebaseFirestore.getInstance();
+        // Add a new document with a generated ID
+        db.collection("user")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Success : ", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText(Register.this,"Success", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Failure : ", "Error adding document", e);
+                    }
+                });
     }
 }
